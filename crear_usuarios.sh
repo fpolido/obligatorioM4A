908c39efb83a01da85cdc636fb375a34e1662cad
fpolido <fpolido14@gmail.com>
2025-11-15 13:31:52 -0300

    Script de bash y Usuarios.txt

--- crear_usuarios.sh
+++ crear_usuarios.sh
@@ -48,3 +48,69 @@ if [[ ! -r "$archivo" || ! -f "$archivo" ]]; then
     echo "ERROR: El archivo no es regular o no tiene permisos de lectura"
     exit 6
 fi
+
+usuarios_creados=0
+
+while IFS=':' read -r nombre comentario home crear_home shell; do
+
+    # Saltar líneas vacías
+    [[ -z "$nombre" ]] && continue
+
+    # Validar cantidad de campos
+    campos=$(echo "$nombre:$comentario:$home:$crear_home:$shell" | awk -F':' '{print NF}')
+    if [[ $campos -ne 5 ]]; then
+        echo "ERROR: Formato incorrecto en el archivo para usuario '$nombre'" >&2
+        exit 7
+    fi
+
+    # Aplicar valores por defecto si falta alguno
+    [[ -z "$comentario" ]] && comentario=""
+    [[ -z "$home" ]] && home="/home/$nombre"
+    [[ -z "$crear_home" ]] && crear_home="SI"
+    [[ -z "$shell" ]] && shell="/bin/bash"
+
+    # Crear el usuario según si hay que asegurar home
+    if [[ "$crear_home" == "NO" ]]; then
+        if [[ ! -d "$home" ]]; then
+            if [[ "$mostrar_info" == true ]]; then
+                echo "ATENCION: el usuario $nombre no pudo ser creado"
+            fi
+            continue
+        else
+            useradd -M -d "$home" -s "$shell" -c "$comentario" "$nombre" 2>/dev/null
+        fi
+    else
+        useradd -m -d "$home" -s "$shell" -c "$comentario" "$nombre" 2>/dev/null
+    fi	
+	
+
+    if [[ $? -eq 0 ]]; then
+        usuarios_creados=$((usuarios_creados + 1))
+
+        # Asignar contraseña si fue indicada
+        if [[ -n "$password" ]]; then
+            echo "$nombre:$password" | chpasswd
+        fi
+
+        # Mostrar info si corresponde
+        if $mostrar_info ; then
+            echo "Usuario $nombre creado con éxito con datos indicados:"
+            [[ -n "$comentario" ]] && echo "Comentario: $comentario" || echo "Comentario: "
+            echo "Dir home: $home"
+            echo "Asegurado existencia directorio home: $crear_home"
+            echo "Shell por defecto: $shell"
+            echo
+        fi
+    else
+        if $mostrar_info ; then
+            echo "ATENCIÓN: El usuario $nombre no pudo ser creado"
+            echo
+        fi
+    fi
+done < "$archivo"
+
+if $mostrar_info ; then
+    echo "Se han creado $usuarios_creados usuarios con éxito."
+fi
+
+exit 0
